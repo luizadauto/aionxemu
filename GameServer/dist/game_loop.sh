e0f49d9f07e43fb77a6fb8245b5b42ac0932fdbb
crash override <crash override@c8e74b4f-e79a-dd47-9b13-efe4965f9f28>
2011-01-22 21:05:43 +0000

    Fixed game_loop.sh and login_loop.sh Errors  -  Thanks to Andy_S

--- GameServer/dist/game_loop.sh
+++ GameServer/dist/game_loop.sh
@@ -3,7 +3,7 @@
 err=1
 until [ $err == 0 ];
 do
-	java -server -Xms128m -Xmx1536m -ea -Xbootclasspath/p:./libs/jsr166.jar -cp ./libs/*:ae-game-1.0.1.jar com.aionengine.gameserver.GameServer > log/stdout.log 2>&1
+	java -server -Xms128m -Xmx1536m -ea -Xbootclasspath/p:./libs/jsr166.jar -cp ./libs/*:ax-game-1.0.1.jar com.aionengine.gameserver.GameServer > log/stdout.log 2>&1
 	err=$?
 	sleep 10
 done
