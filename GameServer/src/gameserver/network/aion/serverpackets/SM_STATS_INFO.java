25b8245274844017fd7b932416d6f546a6ff06e9
ZeroSignal82 <ZeroSignal82@c8e74b4f-e79a-dd47-9b13-efe4965f9f28>
2011-02-11 00:35:16 +0000

    Better Player Stats Info for various stats (port ZeroSignal, cred AJHungary, javaz99, Joel, Unknown). Adds Elemental Resistance Light/Dark, Magic Attack, Magic Critical, Physical/Magical Critical Damage Reduce. Fixes Boost Heal Stat. Corrects some Commenting.

--- GameServer/src/gameserver/network/aion/serverpackets/SM_STATS_INFO.java
+++ GameServer/src/gameserver/network/aion/serverpackets/SM_STATS_INFO.java
@@ -31,8 +31,7 @@ import java.nio.ByteBuffer;
 /**
  * In this packet Server is sending User Info?
  *
- * @author -Nemesiss-
- * @author Luno
+ * @author -Nemesiss-, Luno
  */
 public class SM_STATS_INFO extends AionServerPacket {
     /**
@@ -74,8 +73,8 @@ public class SM_STATS_INFO extends AionServerPacket {
         writeH(buf, pgs.getCurrentStat(StatEnum.WIND_RESISTANCE));// [current wind]
         writeH(buf, pgs.getCurrentStat(StatEnum.EARTH_RESISTANCE));// [current earth]
         writeH(buf, pgs.getCurrentStat(StatEnum.FIRE_RESISTANCE));// [current fire]
-        writeH(buf, 0);// [current unknown resistance]
-        writeH(buf, 0);// [current unknown resistance]
+        writeH(buf, pgs.getCurrentStat(StatEnum.ELEMENTAL_RESISTANCE_LIGHT)); // [current light]
+        writeH(buf, pgs.getCurrentStat(StatEnum.ELEMENTAL_RESISTANCE_DARK)); // [current dark]
 
         writeH(buf, player.getLevel());// [level]
 
@@ -90,7 +89,7 @@ public class SM_STATS_INFO extends AionServerPacket {
 
         writeD(buf, 0); // [unk]
         writeD(buf, pgs.getCurrentStat(StatEnum.MAXHP)); // [max hp]
-        writeD(buf, pls.getCurrentHp());// [unk]
+        writeD(buf, pls.getCurrentHp());// [current hp]
 
         writeD(buf, pgs.getCurrentStat(StatEnum.MAXMP));// [max mana]
         writeD(buf, pls.getCurrentMp());// [current mana]
@@ -111,7 +110,7 @@ public class SM_STATS_INFO extends AionServerPacket {
 
         writeH(buf, pgs.getCurrentStat(StatEnum.PHYSICAL_DEFENSE));// [current pdef]
 
-        writeH(buf, pgs.getCurrentStat(StatEnum.MAIN_HAND_POWER));// [current magic attack ?]
+        writeH(buf, pgs.getCurrentStat(StatEnum.MAGICAL_ATTACK));// [current magic attack ?]
 
         writeH(buf, pgs.getCurrentStat(StatEnum.MAGICAL_RESIST)); // [current mres]
 
@@ -129,7 +128,8 @@ public class SM_STATS_INFO extends AionServerPacket {
 
         writeH(buf, 0);// [unk]
         writeH(buf, pgs.getCurrentStat(StatEnum.MAGICAL_ACCURACY));// [current magic accuracy]
-        writeH(buf, 0); // [current concentration]
+        writeH(buf, pgs.getCurrentStat(StatEnum.MAGICAL_CRITICAL)); // [current crit spell]
+
         writeH(buf, 0); // [old current magic boost location]
 
         writeH(buf, 0);// [unk] 1.9 version
@@ -142,11 +142,11 @@ public class SM_STATS_INFO extends AionServerPacket {
             totalBoostMagicalSkill = 2600;
         writeH(buf, totalBoostMagicalSkill); // [current magic boost] 1.9 version
 
-        writeH(buf, pgs.getCurrentStat(StatEnum.BOOST_HEAL) - 100); // [current boost_heal]
+        writeH(buf, pgs.getCurrentStat(StatEnum.BOOST_HEAL)); // [current boost_heal]
         writeH(buf, pgs.getCurrentStat(StatEnum.CRITICAL_RESIST)); // [current strike resist]
         writeH(buf, pgs.getCurrentStat(StatEnum.MAGICAL_CRITICAL_RESIST));// [current spell resist]
-        writeH(buf, 0);// [unk] 1.9 version
-        writeH(buf, 0);// [unk] 1.9 version
+        writeH(buf, pgs.getCurrentStat(StatEnum.PHYSICAL_CRITICAL_DAMAGE_REDUCE));// [current strike fortitude]
+        writeH(buf, pgs.getCurrentStat(StatEnum.MAGICAL_CRITICAL_DAMAGE_REDUCE));// [current spell fortitude]
         writeH(buf, 20511);// [unk] 1.9 version
 
         writeD(buf, (27 + (player.getCubeSize() * 9)));// [unk]
@@ -174,10 +174,10 @@ public class SM_STATS_INFO extends AionServerPacket {
         writeH(buf, pgs.getBaseStat(StatEnum.WILL));// [base will]
 
         writeH(buf, pgs.getBaseStat(StatEnum.WATER_RESISTANCE));// [base water res]
-        writeH(buf, pgs.getBaseStat(StatEnum.WIND_RESISTANCE));// [base water res]
+        writeH(buf, pgs.getBaseStat(StatEnum.WIND_RESISTANCE));// [base wind res]
 
         writeH(buf, pgs.getBaseStat(StatEnum.EARTH_RESISTANCE));// [base earth resist]
-        writeH(buf, pgs.getBaseStat(StatEnum.FIRE_RESISTANCE));// [base water res]
+        writeH(buf, pgs.getBaseStat(StatEnum.FIRE_RESISTANCE));// [base fire res]
 
         writeD(buf, 0);// [unk]
 
@@ -191,7 +191,7 @@ public class SM_STATS_INFO extends AionServerPacket {
         writeH(buf, pgs.getBaseStat(StatEnum.MAIN_HAND_POWER));// [base main hand attack]
         writeH(buf, pgs.getBaseStat(StatEnum.OFF_HAND_POWER));// [base off hand attack]
 
-        writeH(buf, pgs.getBaseStat(StatEnum.MAIN_HAND_POWER)); // [base magic attack ?]
+        writeH(buf, pgs.getBaseStat(StatEnum.MAGICAL_ATTACK)); // [base magic attack ?]
         writeH(buf, pgs.getBaseStat(StatEnum.PHYSICAL_DEFENSE)); // [base pdef]
 
         writeH(buf, pgs.getBaseStat(StatEnum.MAGICAL_RESIST)); // [base magic res]
@@ -209,7 +209,7 @@ public class SM_STATS_INFO extends AionServerPacket {
         writeH(buf, pgs.getBaseStat(StatEnum.MAIN_HAND_CRITICAL)); // [base main hand crit rate]
         writeH(buf, pgs.getBaseStat(StatEnum.OFF_HAND_CRITICAL)); // [base off hand crit rate]
 
-        writeH(buf, pgs.getCurrentStat(StatEnum.MAGICAL_CRITICAL)); // [base or current MAGICAL crit rate] VERSION 1.9
+        writeH(buf, pgs.getBaseStat(StatEnum.MAGICAL_CRITICAL)); // [base MAGICAL crit rate]
         writeH(buf, 0); // [unk] VERSION 1.9
 
         writeH(buf, pgs.getBaseStat(StatEnum.MAIN_HAND_ACCURACY)); // [base main hand accuracy]
@@ -222,11 +222,10 @@ public class SM_STATS_INFO extends AionServerPacket {
         writeH(buf, 0); // [base concentration]
         writeH(buf, pgs.getBaseStat(StatEnum.MAGICAL_ATTACK) + pgs.getBaseStat(StatEnum.BOOST_MAGICAL_SKILL));// [base magic boost]
 
-        writeH(buf, pgs.getBaseStat(StatEnum.BOOST_HEAL) - 100); // [base boostheal]
+        writeH(buf, pgs.getBaseStat(StatEnum.BOOST_HEAL)); // [base boostheal]
         writeH(buf, pgs.getBaseStat(StatEnum.CRITICAL_RESIST)); // [base strike resist]
         writeH(buf, pgs.getBaseStat(StatEnum.MAGICAL_CRITICAL_RESIST));// [base spell resist]
-        writeH(buf, 0); // [unk] VERSION 1.9
-        writeH(buf, 0); // [unk] VERSION 1.9
-
-	}
+        writeH(buf, pgs.getBaseStat(StatEnum.PHYSICAL_CRITICAL_DAMAGE_REDUCE)); // [base strike fortitude]
+        writeH(buf, pgs.getBaseStat(StatEnum.MAGICAL_CRITICAL_DAMAGE_REDUCE)); // [base spell fortitude]
+    }
 }
