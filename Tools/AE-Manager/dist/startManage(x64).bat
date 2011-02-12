48f0324434d9dfa485b07b3633ce9912f95c1410
crash override <crash override@c8e74b4f-e79a-dd47-9b13-efe4965f9f28>
2011-02-12 03:43:38 +0000

    Added x64 Option for StartManager

---
+++ Tools/AE-Manager/dist/startManage(x64).bat
@@ -0,0 +1,92 @@
+:######################################################################## 
+:# File name: startManager.bat
+:# Edited Last By: vyaslav 
+:# V 1.0 1
+:######################################################################## 
+
+@echo off
+title Manager
+:start
+echo  ______                          ______                                         
+echo /\  _  \  __                    /\  ___\                                        
+echo \ \ \ \ \/\_\    ___     ___    \ \ \____    ___      __   __    ___      __    
+echo  \ \  __ \/\ \  / __`\ /' _ `\   \ \  ___\ /' _ `\  /'__`\/\_\ /' _ `\  /'__`\  
+echo   \ \ \/\ \ \ \/\ \ \ \/\ \/\ \   \ \ \____/\ \/\ \/\ \ \ \/\ \/\ \/\ \/\  __/  
+echo    \ \_\ \_\ \_\ \____/\ \_\ \_\   \ \_____\ \_\ \_\ \___, \ \ \ \_\ \_\ \____\ 
+echo     \/_/\/_/\/_/\/___/  \/_/\/_/    \/_____/\/_/\/_/\/_____ \ \_\/_/\/_/\/____/ 
+echo                                                        /\____\/_/               
+echo                                                        \/____/                  
+echo.
+echo 
+echo.
+
+set JAVA6="auto"
+set JAR=manager.jar
+
+set X86="%ProgramFiles%"
+if %JAVA6% == "auto" (
+  echo Autodetecting java
+  if %X86% NEQ "" (
+    echo Testing x86 folder
+    for /D %%j in ("%ProgramFiles%\jre6*" "%ProgramFiles%\jre1.6.*" "%ProgramFiles%\jdk1.6.*" "%ProgramFiles%\Java\jre6*" "%ProgramFiles%\Java\jre1.6.*" "%ProgramFiles%\Java\jdk1.6.*") do (
+      echo Checking %%j folder
+      if exist "%%j\bin\java.exe" (
+        echo Found java in %%j folder
+        set JAVA6="%%j\bin\java.exe"
+      )
+    )
+  ) else (
+    echo Testing default folder
+    if defined ProgramFiles (
+    for /D %%j in ("%ProgramFiles%\jre6*" "%ProgramFiles%\jre1.6.*" "%ProgramFiles%\jdk1.6.*" "%ProgramFiles%\Java\jre6*" "%ProgramFiles%\Java\jre1.6.*" "%ProgramFiles%\Java\jdk1.6.*") do (
+        echo Checking %%j folder
+        if exist "%%j\bin\java.exe" (
+          echo Found java in %%j folder
+          set JAVA6="%%j\bin\java.exe"
+        )
+      )
+    )
+  )
+)
+
+:rerun
+if %JAVA6% == "auto" (
+  echo ERROR: Java not found!
+  echo Please download and install from java.com
+  pause
+  exit
+) else (
+  echo Starting java from %JAVA6%
+  %JAVA6% -Xms8m -Xmx32m -ea -cp ./libs/*;%JAR% com.aionengine.manager.Manager
+
+  if errorlevel 11 (
+    if not exist %JAR% (
+      echo ========================================
+      echo Warning: %JAR% not not found.
+      echo ========================================
+    )
+
+    echo ERROR: Failed to run %JAR%
+    echo JAVA6=%JAVA6%
+    pause
+    exit
+  )
+
+  if errorlevel 10 (
+    goto rerun
+  )
+
+  if errorlevel 1 (
+    if not exist %JAR% (
+      echo ========================================
+      echo Warning: %JAR% not found.
+      echo ========================================
+    )
+
+    echo ERROR: Failed to run %JAR%
+    echo JAVA6=%JAVA6%
+    pause
+    exit
+  )
+)
+exit
