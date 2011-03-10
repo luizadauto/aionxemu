01f8ff62e012c692fa361b98f2371fbdfd5908fc
versivus <versivus@gmail.com>
2011-03-10 19:42:25 +0000

    Included Survey System Into the aionx_gs.sql

--- GameServer/sql/aionx_gs.sql
+++ GameServer/sql/aionx_gs.sql
@@ -632,3 +632,44 @@ INSERT INTO `npc_shouts` VALUES (798390, 390464, 180);
 INSERT INTO `npc_shouts` VALUES (798391, 390463, 180);
 INSERT INTO `npc_shouts` VALUES (798392, 390466, 180);
 INSERT INTO `npc_shouts` VALUES (798393, 390468, 180);
+
+-- ----------------------------
+-- `player_surveys`
+-- ----------------------------
+DROP TABLE IF EXISTS `player_surveys`;
+CREATE TABLE `player_surveys` (
+  `survey_id` int(11) NOT NULL AUTO_INCREMENT,
+  `player_id` int(11) NOT NULL,
+  `option_id` tinyint(1) NOT NULL,
+  PRIMARY KEY (`survey_id`,`player_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- `surveys`
+-- ----------------------------
+DROP TABLE IF EXISTS `surveys`;
+CREATE TABLE `surveys` (
+  `survey_id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) NOT NULL,
+  `title` varchar(127) NOT NULL,
+  `message` varchar(1023) NOT NULL,
+  `itemId` int(11) DEFAULT NULL,
+  `itemCount` int(11) DEFAULT NULL,
+  `player_level_min` tinyint(1) DEFAULT NULL,
+  `player_level_max` tinyint(1) DEFAULT NULL,
+  `survey_all` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`survey_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- `surveys_option`
+-- ----------------------------
+DROP TABLE IF EXISTS `surveys_option`;
+CREATE TABLE `surveys_option` (
+  `survey_id` int(11) NOT NULL AUTO_INCREMENT,
+  `option_id` tinyint(1) NOT NULL,
+  `option_text` varchar(255) NOT NULL,
+  `itemId` int(11) DEFAULT NULL,
+  `itemCount` int(11) DEFAULT NULL,
+  PRIMARY KEY (`survey_id`,`option_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
